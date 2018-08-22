5c31fcf660458c3d2c5017c783e9c869f2d40426
21961252@qq.com <21961252@qq.com>
2018-08-22 08:47:07 +0800

    弹出进度款

--- src/android/com/zsoftware/update/PgyUpdatePlugin.java
+++ src/android/com/zsoftware/update/PgyUpdatePlugin.java
@@ -47,52 +47,25 @@ public class PgyUpdatePlugin extends CordovaPlugin {
             cordova.getActivity().runOnUiThread(new Runnable() {
                 public void run() {
                    
-                new PgyUpdateManager.Builder()
-                        .setForced(true)                //设置是否强制更新,非自定义回调更新接口此方法有用
-                        .setUserCanRetry(true)         //失败后是否提示重新下载，非自定义下载 apk 回调此方法有用
-                        .setDeleteHistroyApk(true)     // 检查更新前是否删除本地历史 Apk
-                        .setUpdateManagerListener(new UpdateManagerListener() {
-                            @Override
-                            public void onNoUpdateAvailable() {
-                                //没有更新是回调此方法
-                               Toast.makeText(_activity, "已是最新版", Toast.LENGTH_LONG);
-                            }
+                 PgyUpdateManager.register(new UpdateManagerListener() {   // 弃用方法，不推介
+                        @Override
+                        public void onNoUpdateAvailable() {
+                            //检测没有更新的回调
+                            Toast.makeText(_activity, "已是最新版", Toast.LENGTH_LONG);
+                        }
 
-                            @Override
-                            public void onUpdateAvailable(AppBean appBean) {
-                                //没有更新是回调此方法
-                      
+                        @Override
+                        public void onUpdateAvailable(AppBean appBean) {
+                            //调用以下方法，DownloadFileListener 才有效；如果完全使用自己的下载方法，不需要设置DownloadFileListener
+                            PgyUpdateManager.downLoadApk(appBean.getDownloadURL());
+                            callbackContext.success();
+                        }
 
-                                //调用以下方法，DownloadFileListener 才有效；如果完全使用自己的下载方法，不需要设置DownloadFileListener
-                                PgyUpdateManager.downLoadApk(appBean.getDownloadURL());
-                            }
+                        @Override
+                        public void checkUpdateFailed(Exception e) {   //再回调失败的时候，增加了新的接口
 
-                            @Override
-                            public void checkUpdateFailed(Exception e) {
-                                //更新检测失败回调
-                               Toast.makeText(_activity, "检查失败", Toast.LENGTH_LONG);
-
-                            }
-                        })
-                        //注意 ：下载方法调用 PgyUpdateManager.downLoadApk(appBean.getDownloadURL()); 此回调才有效
-                        .setDownloadFileListener(new DownloadFileListener() {   // 使用蒲公英提供的下载方法，这个接口才有效。
-                            @Override
-                            public void downloadFailed() {
-                                //下载失败
-                                Toast.makeText(_activity, "下载失败", Toast.LENGTH_LONG);
-                            }
-
-                            @Override
-                            public void downloadSuccessful(Uri uri) {
-                                PgyUpdateManager.installApk(uri);  // 使用蒲公英提供的安装方法提示用户 安装apk
-                            }
-
-                            @Override
-                            public void onProgressUpdate(Integer... integers) {
-                               
-                            }
-                        })
-                        .register();
+                        }
+                    });
 
                 }
             });
